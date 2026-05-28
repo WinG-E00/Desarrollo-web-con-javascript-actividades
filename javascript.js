189477a3cab33b21e30b9be67408050a6274c41f
WinjeKN <gastonencinawj@gmail.com>
2026-05-28 16:31:58 -0300

    main:Correccion

--- javascript.js
+++ javascript.js
@@ -1,67 +1,58 @@
-// Ejercicio1
+// Ejercicio 1
 const nombre = "Gaston";
 let edad = 21;
-let carrera = "Instituto politecnico";
+let carrera = "Instituto Politecnico";
 let estado = "cursando";
 
 
 // Ejercicio 2
-let nota = Number(prompt());
+let nota = Number(prompt("Ingrese una nota"));
 
-
-if (nota >= 6){
-    return console.log("Aprobado")
+if (nota >= 6) {
+    console.log("Aprobado");
 } else {
-    return console.log("Desaprobado")
+    console.log("Desaprobado");
 }
 
 
-// Ejercicio3
-let edadE3 = Number(prompt());
-
+// Ejercicio 3
+let edadE3 = Number(prompt("Ingrese una edad"));
 
 if (edadE3 < 13) {
-    return console.log("Niño")
-} else if(edadE3 >= 13 && edadE3 <= 17){
-    return console.log("Adolescente")
-} else if(edadE3 >= 18){
-    return console.log("Adulto")
+    console.log("Niño");
+} else if (edadE3 <= 17) {
+    console.log("Adolescente");
+} else {
+    console.log("Adulto");
 }
 
-// Ejercicio4
-let numero = Number(prompt())
 
+// Ejercicio 4
+let numero = Number(prompt("Ingrese un número"));
 
-for(var i = 1; i <= 10; i++){
-    console.log(i*10)
-    
+for (let i = 1; i <= 10; i++) {
+    console.log(i * numero);
 }
 
 
 // Ejercicio 5
-
 let acumuladora = 0;
 
-
-for(var i = 1; i < 100; i ++){
+for (let i = 1; i < 100; i++) {
     acumuladora += i;
 }
 
-console.log(`suma ${acumuladora}`)
+console.log(`Suma: ${acumuladora}`);
 
 
 // Ejercicio 6
-
 for (let i = 1; i <= 20; i++) {
-
-  if (i % 2 === 0) {
-    console.log(i + " es par");
-  } else {
-    console.log(i + " es impar");
-  }
-
+    if (i % 2 === 0) {
+        console.log(i + " es par");
+    } else {
+        console.log(i + " es impar");
+    }
 }
-
 
 
 // Ejercicio 7
@@ -80,34 +71,29 @@ console.log(estudiante.aprueba());
 
 // Ejercicio 8
 let notas = [6, 7, 6, 9];
-
-let acumuladora = 0;
+let sumaNotas = 0;
 
 for (let i = 0; i < notas.length; i++) {
-    acumuladora += notas[i];
+    sumaNotas += notas[i];
 }
 
-console.log(`El promedio es ${acumuladora / notas.length}`);
+console.log(`El promedio es ${sumaNotas / notas.length}`);
 
-//Ejercicio 9
+
+// Ejercicio 9
 let nombreEstudiante = "Gastonn";
 let notasAlumno = [10, 10, 10, 10];
 
 let suma = 0;
-let promedio = suma / notasAlumno.length;
-
 
 for (let i = 0; i < notasAlumno.length; i++) {
     suma += notasAlumno[i];
 }
 
+let promedio = suma / notasAlumno.length;
 
 console.log(
-    `Estudiante: ${nombreEstudiante}
+`Estudiante: ${nombreEstudiante}
 Promedio: ${promedio.toFixed(1)}
 ${promedio >= 6 ? "Aprobado" : "Desaprobado"}`
 );
-
-
-
-
